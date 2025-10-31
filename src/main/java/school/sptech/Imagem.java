3ffb4e6090996dd8221cb0b836000a4aa8254797
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-10-31 12:12:13 -0300

    exercicio terminado!!!

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,57 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+        figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaAreas = 0.0;
+        for(Figura figura : figuras){
+            somaAreas += figura.calcularArea();
+        }
+
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasAreaMaiorQue20 = new ArrayList<>();
+
+        for(Figura figura : figuras){
+            if(figura.calcularArea() > 20){
+                figurasAreaMaiorQue20.add(figura);
+            }
+        }
+
+        return figurasAreaMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadrados = new ArrayList<>();
+
+        for(Figura figura : figuras){
+            if(figura instanceof Quadrado){
+                quadrados.add(figura);
+            }
+        }
+
+        return quadrados;
+    }
+
+    public List<Figura> getFiguras() {
+        return figuras;
+    }
+
+    public void setFiguras(List<Figura> figuras) {
+        this.figuras = figuras;
+    }
 }
